6c8dacdd747284ee252c59fff466d85b004dc3a4
arthur.lecompte <arthur.lecompte14@gmail.com>
2023-12-19 14:47:15 +0100

    feat: init-db env variables check

--- bin/init-db.js
+++ bin/init-db.js
@@ -1,11 +1,29 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+//check for each env variable and throw error if not found
+const requiredEnvVariables = [
+    'DB_HOST',
+    'DB_USER',
+    'DB_PASSWORD',
+    'DB_NAME',
+    'DB_PORT',
+    'DB_DIALECT',
+];
+
+requiredEnvVariables.forEach(envVariable => {
+    if (!process.env[envVariable]) {
+        throw new Error(`Environment variable ${envVariable} is missing. Please check your .env file.`);
+    }
+});
+
+
 const connectionConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
 };
+
 
 async function initDatabase() {
     const connection = await mysql.createConnection(connectionConfig);
